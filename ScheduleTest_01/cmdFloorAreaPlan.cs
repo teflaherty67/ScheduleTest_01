f7e154182df33ab767c8bec8f2490fac5f97b765
Tom E Flaherty <teflaherty@utexas.edu>
2023-09-28 15:40:29 -0500

    test in progress

--- ScheduleTest_01/cmdFloorAreaPlan.cs
+++ ScheduleTest_01/cmdFloorAreaPlan.cs
@@ -23,24 +23,62 @@ namespace ScheduleTest_01
             UIApplication uiapp = commandData.Application;
 
             // this is a variable for the current Revit model
-            Document doc = uiapp.ActiveUIDocument.Document;
+            Document curDoc = uiapp.ActiveUIDocument.Document;
 
             // Your code goes here
-            using (Transaction t = new Transaction(doc))
+            using (Transaction t = new Transaction(curDoc))
             {
                 t.Start("Create Area Plan");
 
-                AreaScheme schemeFloor = Utils.GetAreaSchemeByName(doc, "S Floor");
+                AreaScheme schemeFloor = Utils.GetAreaSchemeByName(curDoc, "S Floor");
                 ElementId schemeFloorId = schemeFloor.Id;
 
-                Level curLevel = Utils.GetLevelByName(doc, "Main Level");
+                Level curLevel = Utils.GetLevelByName(curDoc, "Main Level");
                 ElementId curLevelId = curLevel.Id;
 
-                View vtFloorAreas = Utils.GetViewTemplateByName(doc, "10-Floor Area");
+                View vtFloorAreas = Utils.GetViewTemplateByName(curDoc, "10-Floor Area");
 
-                ViewPlan areaFloor = ViewPlan.CreateAreaPlan(doc, schemeFloorId, curLevelId);
+                ViewPlan areaFloor = ViewPlan.CreateAreaPlan(curDoc, schemeFloorId, curLevelId);
                 areaFloor.Name = "Floor";
                 areaFloor.ViewTemplateId = vtFloorAreas.Id;
+
+                XYZ insStart = new XYZ(0, 0, 0);               
+
+                UV insPoint = new UV(insStart.X, insStart.Y);
+                
+
+                Area areaLiving1 = curDoc.Create.NewArea(areaFloor, insPoint);
+                areaLiving1.Number = "1";
+                areaLiving1.Name = "Living";
+
+                Area areaGarage = curDoc.Create.NewArea(areaFloor, insPoint);
+                areaGarage.Number = "2";
+                areaGarage.Name = "Garage";
+
+                Area areaCoveredPatio = curDoc.Create.NewArea(areaFloor, insPoint);
+                areaCoveredPatio.Number = "3";
+                areaCoveredPatio.Name = "Covered Patio";
+
+                Area areaCoveredPorch = curDoc.Create.NewArea(areaFloor, insPoint);
+                areaCoveredPorch.Number = "4";
+                areaCoveredPorch.Name = "Covered Porch";
+
+                Area areaPorteCochere = curDoc.Create.NewArea(areaFloor, insPoint);
+                areaPorteCochere.Number = "5";
+                areaPorteCochere.Name = "Porte Cochere";
+
+                Area areaPatio = curDoc.Create.NewArea(areaFloor, insPoint);
+                areaPatio.Number = "6";
+                areaPatio.Name = "Patio";
+
+                Area areaPorch = curDoc.Create.NewArea(areaFloor, insPoint);
+                areaPorch.Number = "7";
+                areaPorch.Name = "Porch";
+
+                Area areaOption1 = curDoc.Create.NewArea(areaFloor, insPoint);
+                areaOption1.Number = "8";
+                areaOption1.Name = "Option";
+                areaOption1.LookupParameter("Area Category").Set("Options");
 
                 t.Commit();
             }
